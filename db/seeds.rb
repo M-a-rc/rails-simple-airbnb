058a32723e9c30bcd627cb5acbca069209fbd011
Marc Delesalle <mrc.delesalle@gmail.com>
2022-05-18 13:01:47 +0200

    initial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Flat.create(name: Faker::Address.street_name, address: Faker::Address.street_address, number_of_guests: rand(12), price_per_night: rand(80..400), description: Faker::ChuckNorris.fact)
+end
